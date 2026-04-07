eb427bff7dcc320e55affa9e6568ef29fd2e11f1
Bhumi Sengar <bhumi_sengar@srmap.edu.in>
2026-04-07 19:39:18 +0530

    Added LeetCode 259 solution

---
+++ Array/259-3Sum-Smaller.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int threeSumSmaller(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int count = 0;
+
+    for(int i = 0; i < n - 2; i++) {
+        int left = i + 1, right = n - 1;
+
+        while(left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum < target) {
+                count += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return count;
+}
